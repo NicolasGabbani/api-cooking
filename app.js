cee59d063a6d7e3069fabfc0a484c60756d19c3e
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-05-07 11:32:28 +0200

    remove body-parser

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import './db.js'
 import { router as foodRoutes } from './routes/foodRoutes.js'
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
